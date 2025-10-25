2ba0e062711d108936b483030523dc9d41b632ea
Felipe Bassetto <fsbassetto@rd.com.br>
2025-10-25 11:00:35 -0300

    feat: add deep linking support and README

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,43 +4,39 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { DailyQuoteScreen } from "@screens/DailyQuote/DailyQuote";
 import { LoginScreen } from "@screens/Login/Login";
 
-type AuthStackParamList = {
+type RootStackParamList = {
   Login: undefined;
-};
-
-type AppStackParamList = {
   DailyQuote: undefined;
 };
 
-const AuthStack = createNativeStackNavigator<AuthStackParamList>();
-const AppStack = createNativeStackNavigator<AppStackParamList>();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-const AuthNavigator = () => (
-  <AuthStack.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}
-  >
-    <AuthStack.Screen name="Login" component={LoginScreen} />
-  </AuthStack.Navigator>
-);
-
-const AppNavigatorStack = () => (
-  <AppStack.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}
-  >
-    <AppStack.Screen name="DailyQuote" component={DailyQuoteScreen} />
-  </AppStack.Navigator>
-);
+const linking = {
+  prefixes: ["dailyquote://"],
+  config: {
+    screens: {
+      DailyQuote: "quoteoftheDay",
+      Login: "login",
+    },
+  },
+};
 
 export const AppNavigator = () => {
   const { isAuthenticated } = useAuth();
 
   return (
-    <NavigationContainer>
-      {isAuthenticated ? <AppNavigatorStack /> : <AuthNavigator />}
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        {isAuthenticated ? (
+          <Stack.Screen name="DailyQuote" component={DailyQuoteScreen} />
+        ) : (
+          <Stack.Screen name="Login" component={LoginScreen} />
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
